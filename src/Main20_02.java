7fb65bb76bdb5cffc9f14ba6953c9ce45fb11fc7
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-03 13:16:07 +0100

    add class Main20_02.java

---
+++ src/Main20_02.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main20_02 {
+
+    public static int[] generateArray(int length, int min, int max) {
+      Random random = new Random();
+      int[] arr = new int[length];
+      for (int i = 0; i < length; i++) {
+        arr[i] = min + random.nextInt(max - min);
+      }
+      return arr;
+    }
+
+    public static void reverseArray(int[] arr) {
+      for (int i = 0; i < arr.length / 2; i++) {
+        int temp = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = arr[i];
+        arr[i] = temp;
+      }
+    }
+
+    public static void main(String[] args) {
+      int[] arr = generateArray(10, 1, 100);
+      System.out.println(Arrays.toString(arr));
+      reverseArray(arr);
+      System.out.println(Arrays.toString(arr));
+    }
+}
+
