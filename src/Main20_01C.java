005cd2e3a0fc471984d0bbda8ce88dc7cf68e4b9
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-03 14:04:54 +0100

    add 5 classes Main

---
+++ src/Main20_01C.java
@@ -0,0 +1,30 @@
+public class Main20_01C {
+  //    Написать метод, который добавит один элемент с заданным значением в конец массива
+
+  public static int[] add(int[] arr, int value) {
+    // временный массив для добавления нового элемент
+    int[] temp = new int[arr.length + 1];
+    for (int i = 0; i < arr.length; i++) { // каждый элемент по одному записываем из старого в новый
+      temp[i] = arr[i];
+    }
+    // индекс позиции после последнего элемента всегда равен размеру массива
+    temp[arr.length] = value;
+    // возвращаем массив
+    return temp;
+  }
+
+  public static void printArr(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {1, 3, 5, 7, 9};
+    printArr(arr);
+    System.out.println();
+    arr = add(arr, 11);
+    printArr(arr);
+  }
+
+}
